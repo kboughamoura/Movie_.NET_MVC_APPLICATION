d0c7d98f6ad6bed50bf2c26a26e1832cb10badee
khaled boughammoura <boughamourakhaled11@gmail.com>
2023-03-07 17:24:27 +0100

    Created category Controller

---
+++ Movie_APPLICATION/Controllers/CategoriesController.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Movie_APPLICATION.DAL;
+using Movie_APPLICATION.Models;
+
+namespace Movie_APPLICATION.Controllers
+{
+    public class CategoriesController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public CategoriesController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Categories
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Categories.ToListAsync());
+        }
+
+        // GET: Categories/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var movieCategory = await _context.Categories
+                .FirstOrDefaultAsync(m => m.ID == id);
+            if (movieCategory == null)
+            {
+                return NotFound();
+            }
+
+            return View(movieCategory);
+        }
+
+        // GET: Categories/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Categories/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("ID,CategoryName")] MovieCategory movieCategory)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(movieCategory);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(movieCategory);
+        }
+
+        // GET: Categories/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var movieCategory = await _context.Categories.FindAsync(id);
+            if (movieCategory == null)
+            {
+                return NotFound();
+            }
+            return View(movieCategory);
+        }
+
+        // POST: Categories/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("ID,CategoryName")] MovieCategory movieCategory)
+        {
+            if (id != movieCategory.ID)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(movieCategory);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!MovieCategoryExists(movieCategory.ID))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(movieCategory);
+        }
+
+        // GET: Categories/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var movieCategory = await _context.Categories
+                .FirstOrDefaultAsync(m => m.ID == id);
+            if (movieCategory == null)
+            {
+                return NotFound();
+            }
+
+            return View(movieCategory);
+        }
+
+        // POST: Categories/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var movieCategory = await _context.Categories.FindAsync(id);
+            _context.Categories.Remove(movieCategory);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool MovieCategoryExists(int id)
+        {
+            return _context.Categories.Any(e => e.ID == id);
+        }
+    }
+}
